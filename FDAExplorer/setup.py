2781d8e89e60fa2db44c3bee213ff1170799f442
McClain Thiel <mthiel@nference.com>
2021-06-23 12:12:33 -0400

    pages working?

---
+++ FDAExplorer/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Search FDA 510(k) database for similar application and new products',
+    author='McClain Thiel',
+    license='MIT',
+)
