68c75f450a864c72db34d13280d6265d0d8e2f5b
McClain Thiel <mthiel@nference.com>
2021-06-23 13:11:49 -0400

    i broke then fixed it

---
+++ FDAExplorer/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this project is to build a tool that allows users to easily search the FDA 510(K) patent database in a more effective way.',
+    author='McClain Thiel',
+    license='MIT',
+)
